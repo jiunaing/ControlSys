84c74c18c94b4e66b2eb10ab3274f7f6ba9a01ed
jiunaing <yeapjiunaing@Yeaps-MacBook-Pro.local>
2018-08-17 18:46:31 +0800

    de
    Add more desc

--- myPythonSocket.py
+++ myPythonSocket.py
@@ -1,3 +1,5 @@
+#This is a client socket to connect to the server
+#to 50000 is the port to be decided
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 50000))
